3751dbf8d851cef15f4bf5f5544876c81374ed4e
Shakuriks <legonkovnikita77@gmail.com>
2023-03-05 18:49:26 +0300

    +Lab1 v.2

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,1 +1,14 @@
 print("Hello, world!")
+
+def calculate(x, y, operation):
+    if operation == 'add':
+        return x + y
+    elif operation == 'sub':
+        return x - y
+    elif operation == 'mult':
+        return x * y
+    elif operation == 'div':
+        if y == 0:
+            print("division by zero is impossible")
+        else:
+            return x / y
